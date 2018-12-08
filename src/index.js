4841d1a5a91b9b7396d3a10769460b4914950ef7
A-Y-B <aybwww@gmail.com>
2018-12-09 00:14:08 +0500

    перевел коментарии

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Если вы хотите, чтобы ваше приложение работало в автономном режиме и загружалось быстрее, вы можете изменить
+// отменить регистрацию () для регистрации () ниже. Обратите внимание, что это связано с некоторыми подводными камнями.
+// Узнайте больше о работниках сферы обслуживания: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
